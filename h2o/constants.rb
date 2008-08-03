7c46bd13a9eac8ff36c400f1550ffc626d1ec089
taylor luk <subjective@gmail.com>
2008-08-04 01:38:00 +1000

    implemented with tag, refactored parser constants, hide parser in pretty print

--- h2o/constants.rb
+++ h2o/constants.rb
@@ -6,4 +6,27 @@ module H2o
   VAR_END = '}}'
   COMMENT_START = '{*'
   COMMENT_END = '*}'
+  
+  
+  
+  NAME_RE = /
+    [a-zA-Z_][a-zA-Z0-9_]*
+    (?:\.[a-zA-Z0-9][a-zA-Z0-9_-]*)*
+  /x
+  
+  WHITESPACE_RE = /\s+/m
+
+  PIPE_RE = /\|/
+  SEPERATOR_RE = /,/
+  FILTER_END_RE = /;/
+  STRING_RE = /
+    (?:
+      "([^"\\]*(?:\\.[^"\\]*)*)"
+      |
+      '([^'\\]*(?:\\.[^'\\]*)*)'
+    )
+    /xm
+  
+  NUMBER_RE = /\d+(\.\d*)?/
+  OPERATOR_RE = /(?:!|>|<|=|>=|<=|!=|==|=|and|not|or)/
 end
